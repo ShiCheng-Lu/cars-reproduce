abe84867bedfbe78e8068f43f09fc2a8972db9f1
ShiCheng Lu <shichenglu0708@gmail.com>
2022-10-02 02:16:55 -0400

    cars

---
+++ car.py
@@ -0,0 +1,145 @@
+import math
+from time import sleep
+from turtle import back
+import numpy as np
+import pygame
+
+class Car:
+    def __init__(self, points=(0, 0), dir=0):
+        self.point = points
+        self.dir = dir
+
+    def drive(self, left, right):
+        self.point = (self.point[0] + math.cos(self.dir), self.point[1] + math.sin(self.dir))
+        self.dir += left - right
+    
+    def draw(self, surface, color):
+        pygame.draw.circle(surface, color, center(self.point), 3)
+        pygame.draw.line(surface, color, center(self.point), center(extend(self.point, self.dir, 1)))
+
+class Road:
+    def __init__(self, outerbound = None, innerbound = None):
+        self.outerbound = outerbound # np array of points
+        self.innerbound = innerbound # np array of points
+        if (self.outerbound and self.innerbound):
+            self.calcStart()
+    
+    def calcStart(self):
+        self.start = ((self.innerbound[0][0] + self.outerbound[0][0]) / 2, (self.innerbound[0][1] + self.outerbound[0][1]) / 2)
+        self.dir = math.atan2(self.innerbound[1][1] - self.innerbound[0][1], self.innerbound[1][0] - self.innerbound[0][0])
+        return (self.start, self.dir)
+    
+    def generate_rand(self, points):
+        # road = []
+        # loc = (0, 0)
+        # dir = 0
+        # for i in range(points):
+        #     road.append(loc)
+
+        #     dir += np.random.normal()
+        #     loc = (loc[0] + math.cos(dir), loc[1] + math.sin(dir))
+        
+        # width = 1
+
+        self.outerbound = []
+        self.innerbound = []
+
+        # last_point = road[-1]
+        # for point in road:
+        #     midpoint = ((point[0] + last_point[0]) / 2, (point[1] + last_point[1]) / 2)
+            
+        #     slope = 1
+
+        #     self.outerbound.append((midpoint[0], midpoint[1]))
+        #     self.innerbound.append((midpoint[0], midpoint[1]))
+
+        _2pi = 2 * math.pi
+        for i in range(points):
+            theta = _2pi * i / points
+            val = 3 * math.sin(theta * 4) + 13
+
+            self.outerbound.append((math.sin(theta) * val, math.cos(theta) * val))
+            self.innerbound.append((math.sin(theta) * (val + 4), math.cos(theta) * (val + 4)))
+
+        # start is in the middle of first two points in outer/inner bound
+        self.calcStart()
+        return self
+
+    def draw(self, surface, color):
+        for bound in [self.innerbound, self.outerbound]:
+            last_point = bound[-1]
+            for point in bound:
+                pygame.draw.line(surface, color, center(point), center(last_point))
+                last_point = point
+    
+    def sensors(self, car):
+        
+        sens = [math.inf, math.inf, math.inf]
+
+        sens_dir_offset = [-1, 0, 1]
+
+        for bound in [self.innerbound, self.outerbound]:
+            last_point = bound[-1]
+            for point in bound:
+                for i in range(len(sens_dir_offset)):
+                    d = rayLineIntersect((car.point, car.dir + sens_dir_offset[i]), (last_point, point))
+                    if (d and d < sens[i]):
+                        sens[i] = d
+                last_point = point
+
+        return (sens[0], sens[1], sens[2])
+
+def rayLineIntersect(ray, line):
+    '''
+    return distance when ray intersects line, or None if does not intersect
+    ray = ((x, y), dir)
+    line = ((x, y), (x, y))
+    '''
+    dy = math.sin(ray[1])
+    dx = math.cos(ray[1])
+    
+    lineXdiff = line[1][0] - line[0][0]
+    lineYdiff = line[1][1] - line[0][1]
+
+    rayXdiff = line[0][0] - ray[0][0]
+    rayYdiff = line[0][1] - ray[0][1]
+    
+    a_num = lineXdiff * rayYdiff - lineYdiff * rayXdiff
+    b_num = dx * rayYdiff - dy * rayXdiff
+
+    denum = lineXdiff * dy - lineYdiff * dx
+    a = a_num / denum # dist from ray to intersect
+    b = b_num / denum # dist from point a of line
+    
+    return a if (a > 0 and b > 0 and b < 1) else None
+
+def extend(point, dir, size):
+    return (point[0] + math.cos(dir) * size, point[1] + math.sin(dir) * size) 
+
+def center(point):
+    _scale = (15, -15)
+    _offset = (400, 300)
+    return (point[0] * _scale[0] + _offset[0], point[1] * _scale[1] + _offset[1])
+
+def main():
+    pygame.init()
+    pygame.display.set_caption('Quick Start')
+    window_surface = pygame.display.set_mode((800, 600))
+    background = pygame.Surface((800, 600))
+
+    road = Road().generate_rand(50)
+
+    is_running = True
+    while is_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+                print("yes")
+
+        road.draw(background, '#ffffff')
+
+        window_surface.blit(background, (0, 0))
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
---
+++ ml.py
@@ -0,0 +1,204 @@
+# TensorFlow and tf.keras
+import math
+from time import sleep, time_ns
+import tensorflow as tf
+# Helper libraries
+import numpy as np
+import car
+import pygame
+import threading
+
+import multiprocessing as mp
+
+rng = np.random.default_rng()
+
+class GeneticAlgorithm:
+    def return_none():
+        return None
+
+    def __init__(self, create_model=return_none, population=1, fitness=None, sim=None):
+        self.population = population
+        self.create_model = create_model
+        self.models = [create_model() for _ in range(population)]
+        self.scores = np.zeros(population) # numpy array
+        self.fitness = fitness
+        self.sim = sim
+    
+    def generation(self, count = 1):
+        '''
+        simulate one generation
+        '''
+        top_10p = int(np.ceil(self.population / 10.0))
+        for gen in range(count):
+            # calculate scores
+            if self.sim:
+                self.scores = self.sim(self.models)
+            else:
+                for i in range(self.population):
+                    self.scores[i] = self.fitness(self.models[i])
+
+            print(np.max(self.scores))
+
+            # calculate weights for the next generation
+            best_models = self.best(2)
+            p1 = best_models[0].get_weights()
+            p2 = best_models[1].get_weights()
+
+            # keep top two for next gen
+            self.models[0].set_weights(p1)
+            self.models[1].set_weights(p2)
+
+            for i in range(2, self.population):
+                weights = self.mutate(self.crossover(p1, p2))
+                self.models[i].set_weights(weights)
+
+    def best(self, count = 1) -> list[tf.keras.Model]:
+        '''
+        return the best model from current generation
+        '''
+        indices = np.argpartition(self.scores, -count)[-count:]
+        
+        return [self.models[i] for i in indices]
+
+    def mutate(self, w: list[np.ndarray]):
+        for i in range(len(w)):
+            w[i] += rng.normal(scale=0.01,size=w[i].shape)
+
+        return w
+    
+    def crossover(self, w1: list[np.ndarray], w2: list[np.ndarray]):
+        w = [None for i in range(len(w1))]
+        for i in range(len(w1)):
+            select = np.random.randint(2, size=w1[i].shape)
+            w[i] = w1[i] * select + w2[i] * (1 - select)
+        return w
+    
+    def save(self, folder):
+        '''
+        save all models
+        '''
+        for i in range(self.population):
+            self.models[i].save(f"{folder}/{i}")
+        
+    
+    def load(self, folder):
+        self.models = []
+        for i in range(self.population):
+            model = tf.keras.models.load_model(f"{folder}/{i}")
+            self.models.append(model)
+        return self
+
+def create_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(3, input_shape=(3,), activation='relu'),
+        tf.keras.layers.Dense(10, input_shape=(3,), activation='relu'),
+        tf.keras.layers.Dense(2, input_shape=(10,), activation='sigmoid')
+    ])
+
+    model.compile(loss='mse',optimizer='adam')
+    return model
+
+def predict(model, inputs):
+    return model(np.atleast_2d(np.asarray(inputs)))[0]
+
+r = car.Road().generate_rand(50)
+
+def crash(sens):
+    return sens[1] == math.inf or sens[1] <= 1
+
+def fitness(model):
+    c = car.Car(r.start, r.dir)
+    for i in range(200):
+        sens = car.sensors(c, r)
+
+        if (crash(sens)): # check crash
+            break
+
+        a = predict(model, sens)
+        c.drive(a[0], a[1])
+
+    return i
+
+draw_surface = None
+pool = None
+
+# multi threading for collision calculations
+def getSensor(c):
+    # if (scores[index] != 0):
+    #     return None
+    return r.sensors(c)
+
+def sim(models):
+    length = len(models)
+    cars = [car.Car(r.start, r.dir) for i in range(length)]
+    scores = np.zeros(length)
+
+    time_last = time_ns()
+
+    def draw():
+        draw_surface.fill('#000000')
+        r.draw(draw_surface, '#ffffff')
+        for m in range(length):
+            cars[m].draw(draw_surface, '#00ff00')
+
+    for i in range(1, 150):
+        sensors = pool.map(getSensor, cars)
+        
+        for m in range(length):
+            if (scores[m] != 0):
+                continue
+            if (crash(sensors[m])): # check crash
+                scores[m] = i
+                continue
+
+            a = predict(models[m], sensors[m])
+            cars[m].drive(a[0], a[1])
+
+        draw()
+        
+        if np.all(scores):
+            break
+
+    return scores
+
+def ml_main():
+    x = GeneticAlgorithm(create_model, population=30, fitness=fitness, sim=sim).load("models")
+
+    for i in range(20):
+        x.generation()
+
+    x.save("models")
+
+    print("waiting for pygame")
+
+
+def main():
+    global pygame_is_running
+    global draw_surface
+    global pool
+
+    pool = mp.Pool(8)
+
+    pygame.init()
+    pygame.display.set_caption('Quick Start')
+    window_surface = pygame.display.set_mode((800, 600))
+    background = pygame.Surface((800, 600))
+    pygame_is_running = True
+    draw_surface = background
+
+    # start ml thread
+    ml_thread = threading.Thread(target=ml_main)
+    ml_thread.start()
+
+    while pygame_is_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame_is_running = False
+
+        window_surface.blit(background, (0, 0))
+        pygame.display.update()
+
+    ml_thread.join()
+
+if __name__ == "__main__":
+    main()
